76107f3bcd5f7421938e8adac302d99977951125
broof716 <broof716@gmail.com>
2019-09-30 07:36:35 +0000

    adding estate model

---
+++ db/migrate/20190930073505_create_estates.rb
@@ -0,0 +1,10 @@
+class CreateEstates < ActiveRecord::Migration[5.2]
+  def change
+    create_table :estates do |t|
+      t.string :name
+      t.text :description
+      t.string :address
+      t.timestamps
+    end
+  end
+end
