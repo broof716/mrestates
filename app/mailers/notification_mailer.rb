14a6fcc05b575b8acde51ec904aa6c79c31c2f95
broof716 <broof716@gmail.com>
2019-10-04 05:42:10 +0000

    adding email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,5 +1,5 @@
 class NotificationMailer < ApplicationMailer
-  default from: "no-reply@nomsterapp.com"
+  default from: "no-reply@mrestateapp.com"
 
   def comment_added(comment)
     @estate = comment.estate
